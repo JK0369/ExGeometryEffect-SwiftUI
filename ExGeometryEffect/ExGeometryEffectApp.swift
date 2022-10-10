e4dda5468780f21605c40bbcddf584b255a1eaaf
jake <palatable7@naver.com>
2022-10-10 11:40:02 +0900

    Initial Commit

---
+++ ExGeometryEffect/ExGeometryEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExGeometryEffectApp.swift
+//  ExGeometryEffect
+//
+//  Created by 김종권 on 2022/10/10.
+//
+
+import SwiftUI
+
+@main
+struct ExGeometryEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
